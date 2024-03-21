745284c0ec0a947ebd4f7d48b4b023ec52dee6ac
UteniyazovSalamat <sss19855891@gmail.com>
2024-03-20 19:06:45 -0600

    learn do..while statement

--- js/main.js
+++ js/main.js
@@ -1,14 +1,5 @@
-const arr = [1, 4, 8, 7];
-
-for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > 5) {
-        break;
-    }
-    console.log(arr[i]);
-}
-
-let i = 0;
-while (arr[i] <= 5 && i < arr.length) {
-    console.log(arr[i]);
-    i++;
-}
+let i = 10;
+do {
+    console.log(i);
+    i--;
+} while (i > 5);
