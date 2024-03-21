3b33d04e662cde5c1fbee1099e45ce1e01b15cca
UteniyazovSalamat <sss19855891@gmail.com>
2024-03-20 18:44:14 -0600

    learn while statement

--- js/main.js
+++ js/main.js
@@ -1,24 +1,14 @@
-for (let i = 1; i < 5; i++) {
-    console.log(`Вывод - ${i}`);
+const arr = [1, 4, 8, 7];
+
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 5) {
+        break;
+    }
+    console.log(arr[i]);
 }
 
-let i = 1;
-while (i < 5) {
-    console.log(`Вывод - ${i}`);
+let i = 0;
+while (arr[i] <= 5 && i < arr.length) {
+    console.log(arr[i]);
     i++;
 }
-
-// const arr = [1, 4, 8, 7];
-
-// for (let i = 0; i < arr.length; i++) {
-//     if (arr[i] > 5) {
-//         break;
-//     }
-//     console.log(arr[i]);
-// }
-
-// let i = 0;
-// while (arr[i] <= 5 && i < arr.length) {
-//     console.log(arr[i]);
-//     i++;
-// }
